1ee2a3b605312f1f455584c0cfc4f6240f48923f
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2023-05-06 16:31:17 +0530

    used union of string literals children props , children props where the type is ReactNode and optional props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -3,17 +3,18 @@ import React from "react";
 // use types when building applications and interfaces when building libraries
 type GreetProps = {
   name: string;
-  messageCount: number;
+  messageCount?: number; // this tell typescript that messageCount prop is optional
   isLoggedIn: boolean;
 };
 
 const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props;
   return (
     <div>
       <h2>
         {props.isLoggedIn
           ? `Welcome ${props.name}! You have 
-          ${props.messageCount} unread messages`
+          ${messageCount} unread messages`
           : "Welcome guest"}
       </h2>
     </div>
